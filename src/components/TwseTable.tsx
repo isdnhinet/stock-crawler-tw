148023030d5e889fd7bb52f009cf3100d6fc7c0e
isdnhinet <isdnhinet@gmail.com>
2026-06-01 10:54:23 +0800

    add table sort

--- src/components/TwseTable.tsx
+++ src/components/TwseTable.tsx
@@ -1,31 +1,55 @@
 import { useTwseQuery } from "../hooks/useTwseQuery";
 import { BaseUrl } from "../constants/baseUrl";
+import { toObjectArray } from "../utils/toObjectArray";
+import { useMemo, useState } from "react";
 
-interface Props {
+type Props = {
     path: typeof BaseUrl[keyof typeof BaseUrl];
     params: Record<string, string>;
+};
+
+function parseValue(v: string) {
+    if (!v || v === '--') return 0;
+    return Number(v.replace(/,/g,""));
 }
 
 export function TwseTable({ path, params }: Props) {
     const { result, status } = useTwseQuery(path, params);
+    const objData = toObjectArray(result.fields, result.data);
+    const [sortField, setSortField] = useState<string>("證券代號");
+    const sortedData = useMemo(() => {
+        if (!objData.length) return [];
+        const data = [...objData];
+        return data.sort((a, b) => {
+            const aVal = parseValue(a[sortField]);
+            const bVal = parseValue(b[sortField]);
+
+            return bVal - aVal;
+        });
+    }, [objData, sortField]);
 
     if(status.loading) return <div>Loading...</div>
     if(status.error) return <div>Error: {status.error}</div>
-
     return (
-        <table className="text-sky-50">
-            <thead>
+        <table className="bg-amber-50">
+             <thead>
                 <tr>
-                    {result.fields.map((field, idx) => (
-                        <th key={idx}>{field}</th>
+                    {result.fields.map((field) => (
+                        <th 
+                            key={field} 
+                            onClick={() => {setSortField(field);}}
+                            style={{cursor: "pointer"}}
+                        >
+                            {field}
+                        </th>
                     ))}
                 </tr>
             </thead>
             <tbody>
-                {result.data.map((row, rowIdx) => (
+               {sortedData.map((row, rowIdx) => (
                     <tr key={rowIdx}>
-                        {row.map((cell, cellIdx) => (
-                            <td key={cellIdx}>{cell}</td>
+                        {result.fields.map((field) => (
+                            <td key={field}>{row[field]}</td>
                         ))}
                     </tr>
                 ))}
